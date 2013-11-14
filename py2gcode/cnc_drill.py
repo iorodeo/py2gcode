a0bdf827f8342b5c88df5b00cbb4e60bcc485ebc
iorodeo <iorodeo@localhost>
2013-11-13 19:19:45 -0800

    Added PeckDrill to cnc_drill.

--- py2gcode/cnc_drill.py
+++ py2gcode/cnc_drill.py
@@ -109,6 +109,34 @@ class SimpleDrill(DrillBase):
         self.addEndComment()
 
 
+class PeckDrill(DrillBase):
+
+    def __init__(self,param):
+        super(PeckDrill,self).__init__(param)
+
+    def makeListOfCmds(self):
+        self.listOfCmds = []
+        cx = float(self.param['centerX'])
+        cy = float(self.param['centerY'])
+        startZ = float(self.param['startZ'])
+        stopZ = float(self.param['stopZ'])
+        stepZ =  float(self.param['stepZ'])
+        assert stepZ>=0,'stepZ must be >= 0'
+        try:
+            startDwell = self.param['startDwell']
+        except KeyError:
+            startDwell = 0.0
+        self.addStartComment()
+        self.addRapidMoveToSafeZ()
+        self.addRapidMoveToPos(x=cx,y=cy,comment='drill x,y')
+        self.addDwell(startDwell)
+        self.addMoveToStartZ()
+        drillCmd = gcode_cmd.PeckDrillCycle(x=cx,y=cy,z=stopZ,r=startZ,q=stepZ)
+        self.listOfCmds.append(drillCmd)
+        self.addRapidMoveToSafeZ()
+        self.addEndComment()
+
+
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
 
@@ -118,16 +146,31 @@ if __name__ == '__main__':
     prog.add(gcode_cmd.Space())
     prog.add(gcode_cmd.FeedRate(2.0))
 
-    param = {
-            'centerX': 0.5,
-            'centerY': 0.2,
-            'startZ': 0.02,
-            'stopZ' : -0.5,
-            'safeZ' : 0.5,
-            'startDwell' : 2.0,
-            }
+    if 0:
+        param = {
+                'centerX': 0.5,
+                'centerY': 0.2,
+                'startZ': 0.02,
+                'stopZ' : -0.5,
+                'safeZ' : 0.5,
+                'startDwell' : 2.0,
+                }
 
-    drill = SimpleDrill(param)
+        drill = SimpleDrill(param)
+
+    if 1:
+        param = {
+                'centerX': 0.5,
+                'centerY': 0.2,
+                'startZ': 0.02,
+                'stopZ' : -0.5,
+                'safeZ' : 0.5,
+                'stepZ' : 0.05,
+                'startDwell' : 2.0,
+                }
+
+        drill = PeckDrill(param)
+
 
     prog.add(drill)
     prog.add(gcode_cmd.Space())
